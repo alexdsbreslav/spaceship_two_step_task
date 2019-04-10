1c2764f71346016e60bfb414069a54cad1bad53b
Alex Stine <ads48@duke.edu>
2019-04-10 15:45:43 -0400

    Update start file for new version of task

---
+++ start.m
@@ -0,0 +1,352 @@
+% The code that this is based on was initially written for Konovalov (2016) Nature Communications.
+% The original code was shared with me and I have maintained some of the basic structure
+% and notation; however, I have substantially altered the code for my own purposes.
+
+% Please do not share or use this code without my written permission.
+% Author: Alex Breslav
+
+function start
+
+% clear everything from the workspace; everything is saved in the initialization_struct
+Screen('CloseAll');
+FlushEvents;
+
+% ------------------------------------------------------------------------------
+% ------------------------------------------------------------------------------
+% ---- Flexible parameters that need to be checked
+% ---- Task specificiations
+num_trials_practice = 2;
+num_trials_main_task = 2;
+
+% --- File specifications
+% --- get input from user on comptuer system
+comp_os = input(['\n\n' ...
+  'Are you on a Mac or a PC?' '\n' ...
+  '1 = Mac' '\n' ...
+  '0 = PC' '\n' ...
+  'Response: ' ]);
+
+if ~ismember(comp_os, [0 1])
+    disp('Invalid entry, please try again.')
+    sca; return
+end
+
+if comp_os == 1
+  file_root = '/Users/alex/OneDrive - Duke University/1_research/2_mdt_thriving/4_raw_data'; % for Alex's computer
+  sl = '/'; % for OSX
+else
+  file_root = '\Users\ads48\Documents\MDT project files\run1\raw data\matlab'; % for the eye-tracker
+  sl = '\'; % for Windows
+end
+
+% --- Check that file root is correct
+file_root_cor = input(['Is the file root correct?' '\n' ...
+file_root '\n\n' ...
+'0 = No; restart the function' '\n' ...
+'1 = Yes' '\n' ...
+'Response: ']);
+
+if ~ismember(start_where, [0 1 2 3 4 5])
+  disp('Invalid entry, please try again.')
+  sca; return
+end
+
+if file_root_cor == 0
+   disp([fprintf('\n') ...
+   'OK, fix the file root and restart the function to try again'])
+   sca; return
+end
+
+% ---- Text formatting specifications
+textsize_directions = 30;
+textsize_allergy_wanting_rateGuide = 20;
+textsize_fixcross = 40;
+textsize_countdown = 20;
+
+% ---- loading bar formatting
+load_bar_dimensions = [400, 15];
+
+% ---- iti distributions N~(mean, sd)
+win_iti = [5, 0.5];
+loss_iti = [3, 0.5];
+
+% ------------------------------------------------------------------------------
+% ------------------------------------------------------------------------------
+
+sub = input('Nest ID: '); %keep sub number as a string so we can validate easily below
+
+% create subject folder in the raw data folder
+filename_subnum = pad(num2str(sub), 4, 'left', '0');
+data_file_path = [file_root sl 'sub' filename_subnum];
+[~, msg, ~] = mkdir(data_file_path);
+
+folder_already_exists = strcmp(msg, 'Directory already exists.');
+
+if folder_already_exists
+   sub_exists = input(['\n\n' ...
+   'Subject' filename_subnum ' already exists. Do you want to enter a new subject number?' '\n\n' ...
+     '1 = Yes, I''ll restart and enter a new subject number.' '\n' ...
+     '0 = No, I need to alter this subject''s data' '\n' ...
+     'Response: ' ]);
+
+     if ~ismember(sub_exists, [0 1])
+       disp('Invalid entry, please try again.')
+       sca; return
+     end
+else
+   sub_exists = 99;
+end
+
+
+if sub_exists == 1
+   disp([ fprintf('\n') ...
+   'OK, you should restart the function to try again'])
+   sca; return
+
+elseif sub_exists == 0
+    load([data_file_path sl 'initialization_structure.mat']);
+
+    start_where = input(['Where do you want to start?' '\n' ...
+    'You will overwrite any existing data on and after the place you choose.' '\n\n' ...
+    '0 = CANCEL and restart the function' '\n' ...
+    '1 = Tutorial' '\n' ...
+    '2 = Practice Rounds' '\n' ...
+    '3 = Test Rounds' '\n' ...
+    'Response: ']);
+
+    if ~ismember(start_where, [0 1 2 3])
+      disp('Invalid entry, please try again.')
+      sca; return
+    end
+
+    if start_where == 0
+       disp([fprintf('\n') ...
+       'OK, you should restart the function to try again'])
+       sca; return
+    end
+
+else
+    start_where = 1;
+
+    % Enter the condition that the subject is in
+    condition = input(['\n\n' ...
+      'What condition is this subject in?' '\n' ...
+      '1 = Food Condition' '\n' ...
+      '2 = Sticker Condition' '\n' ...
+      'Response: ' ]);
+
+    if ~ismember(comp_os, [1 2])
+        disp('Invalid entry, please try again.')
+        sca; return
+    end
+
+    if condition == 1
+      % pick salty food
+      img_files = dir(['food_images' sl '*.png']);
+      img_file_names = {img_files(1:length(img_files)).name}';
+      img_file_index = find(contains(img_file_names, 'salt'))';
+
+      food_salt = input(['Select the salty food for this participant.' '\n' ...
+      'Please select one of the following foods.' '\n\n' ...
+      num2str(img_file_index(1)) ' = ' img_file_names{img_file_index(1)} '\n' ...
+      num2str(img_file_index(2)) ' = ' img_file_names{img_file_index(2)} '\n' ...
+      num2str(img_file_index(3)) ' = ' img_file_names{img_file_index(3)} '\n' ...
+      num2str(img_file_index(4)) ' = ' img_file_names{img_file_index(4)} '\n' ...
+      num2str(img_file_index(5)) ' = ' img_file_names{img_file_index(5)} '\n' ...
+      'Response: ']);
+
+      initialization_struct.food_salt = cellstr(img_file_names{food_salt});
+
+      % pick sweet food
+      img_file_names = {img_files(1:length(img_files)).name}';
+      img_file_index = find(contains(img_file_names, 'sweet'))';
+
+      food_sweet = input(['Select the sweet food for this participant.' '\n' ...
+      'Please select one of the following foods.' '\n\n' ...
+      num2str(img_file_index(1)) ' = ' img_file_names{img_file_index(1)} '\n' ...
+      num2str(img_file_index(2)) ' = ' img_file_names{img_file_index(2)} '\n' ...
+      num2str(img_file_index(3)) ' = ' img_file_names{img_file_index(3)} '\n' ...
+      num2str(img_file_index(4)) ' = ' img_file_names{img_file_index(4)} '\n' ...
+      num2str(img_file_index(5)) ' = ' img_file_names{img_file_index(5)} '\n' ...
+      'Response: ']);
+
+      initialization_struct.food_sweet = cellstr(img_file_names{food_sweet});
+      save([data_file_path sl 'initialization_structure'], 'initialization_struct', '-v6')
+
+      % redo the selection of left/right food
+      initialization_struct.foods_selected_by_comp = 0; %indicate that the foods were not selected by computer
+
+      if initialization_struct.sweet_loc_left == 1
+      initialization_struct.left_item = initialization_struct.food_sweet{1}(7:end-4);
+      initialization_struct.right_item = initialization_struct.food_salt{1}(6:end-4);
+      else
+      initialization_struct.right_item = initialization_struct.food_sweet{1}(7:end-4);
+      initialization_struct.left_item = initialization_struct.food_salt{1}(6:end-4);
+      end
+
+      save([data_file_path sl 'initialization_structure'], 'initialization_struct', '-v6')
+    else
+      % this is where stuff for the sticker condition needs to go that I haven't figure out yet
+    end
+
+    % --- Double check everything
+    double_check = input(['\n\n' ...
+      'Nest ID = ' sub '\n' ...
+      'Condition = ' condition '\n' ...
+      'Left item = ' initialization_struct.left_item '\n' ...
+      'Right item = ' initialization_struct.right_item '\n' ...
+      '0 = I need to fix something; restart the function.' '\n' ...
+      '1 = This is correct; continue.' '\n' ...
+      'Response: ' ]);
+
+    if ~ismember(double_check, [0 1])
+      disp('Invalid entry, please try again.')
+      sca; return
+    end
+
+    if double_check == 0
+       disp([fprintf('\n') ...
+       'OK, you should restart the function to try again'])
+       sca; return
+    end
+
+    % shuffle the rng and save the seed
+    rng('shuffle');
+    init_rng_seed = rng;
+    init_rng_seed = init_rng_seed.Seed;
+
+    % create stimuli structure
+    initialization_struct = struct;
+    initialization_struct.sub = sub; % save the subject number into the structure
+    initialization_struct.data_file_path = data_file_path; % save the data file path as well
+    initialization_struct.rng_seed = init_rng_seed; % save the rng seed for the initialization_structure
+    initialization_struct.condition = condition; % save the condition that the subject was randomized into
+
+    % stimuli sets
+    symbols = {'b', 'e', 'i', 'inf', 'l', 'n', 'o', 'r', 'ri', 'to', 'u', 'w'};
+    prac_symbols = {'4pt', '5pt', 'bolt', 'cir', 'pent', 'tri'};
+    step1_colors = {'white', 'grey', 'dark_grey'};
+    step2_color_pairs = {'red_blue', 'orange_purple', 'yellow_green'};
+    step2_color = {'warm', 'cool'};
+
+    % create shuffled arrays of each of the symbols and colors
+    initialization_struct.stim_color_step1 = step1_colors(randperm(numel(step1_colors)));
+    initialization_struct.stim_colors_step2 = step2_color_pairs(randperm(numel(step2_color_pairs)));
+    initialization_struct.stim_step2_color_select = step2_color(randperm(numel(step2_color)));
+    initialization_struct.stim_prac_symbol = prac_symbols(randperm(numel(prac_symbols)));
+    initialization_struct.stim_symbol = symbols(randperm(numel(symbols)));
+
+    % This was randomized when there was more than 1 block
+    % however the code still uses this to differentiate between practice and main experiment
+    initialization_struct.block = [0 1];
+
+    % randomize the location of the foods
+    sweet_loc_left = randi([0,1]);
+    initialization_struct.sweet_loc_left = sweet_loc_left;
+
+    % input the number of trials per block; 1 = practice trials, 2 = experimental blocks
+    initialization_struct.num_trials = [num_trials_practice num_trials_main_task];
+
+    % set the file root and backslash vs. forwardslash convention
+    initialization_struct.file_root = file_root;
+    initialization_struct.slash_convention = sl;
+
+    % set the text formatting specs
+    initialization_struct.textsize_directions = textsize_directions;
+    initialization_struct.textsize_allergy_wanting_rateGuide = textsize_allergy_wanting_rateGuide;
+    initialization_struct.textsize_fixcross = textsize_fixcross;
+    initialization_struct.textsize_countdown = textsize_countdown;
+
+    % set the load bar formaating
+    initialization_struct.load_bar_dimensions = load_bar_dimensions;
+
+    % create the ITIs
+    iti_init = zeros(150,6);
+    % loss iti
+    iti_init(:,1) = normrnd(loss_iti(1),loss_iti(2),150,1);
+    % win iti
+    iti_init(:,2) = normrnd(win_iti(1),win_iti(2),150,1);
+    % number of frames per iti; subtract 24 frames because the chosen stimulus is shown for 1 second before the loading bar
+    iti_init(:,3:4) = floor(iti_init(:,1:2)*24) - 24;
+    %number of pixels per frame
+    iti_init(:,5:6) = (ones(150,2)*load_bar_dimensions(1))./iti_init(:,3:4);
+    initialization_struct.iti_init = iti_init;
+
+    % load the walk
+    load(['walks.mat']);
+    walk_idx = randi(length(walks.payoff_prob));
+    initialization_struct.walk_idx = walk_idx;
+    initialization_struct.payoff_prob = walks.payoff_prob(:,:,walk_idx);
+    initialization_struct.walk_seed = walks.seed(walk_idx);
+
+    save([data_file_path sl 'initialization_structure'], 'initialization_struct', '-v6')
+end
+
+if start_where == 1
+% ---- 1: Tutorial
+    exit_flag = tutorial_v4(initialization_struct);
+
+    if exit_flag == 1
+        disp('The script was exited because ESCAPE was pressed')
+        sca; return
+    end
+end
+
+if start_where <= 2
+% ---- 2: practice trials (Block 0 in code)
+    if exist([data_file_path sl 'tutorial_timing.mat']) == 2
+        load([data_file_path sl 'tutorial_timing.mat'])
+        exit_flag = main_task(initialization_struct, initialization_struct.num_trials(1), initialization_struct.block(1), tutorial_timing_struct);
+    else
+        exit_flag = main_task(initialization_struct, initialization_struct.num_trials(1), initialization_struct.block(1));
+    end
+
+    if exit_flag == 1
+        disp('The script was exited because ESCAPE was pressed')
+        sca; return
+    end
+end
+
+if start_where <= 3
+% ---- 3: main experiment trials
+% ---- space prepped?
+    if condition == 1
+      reward_bowl_prep = input(['\n\n' ...
+        'Left Food = ' initialization_struct.left_item '\n'...
+        'Right Food = ' initialization_struct.right_item '\n\n' ...
+        '1 = Food is set up/participant has water; continue.' '\n' ...
+        '0 = I need to fix something; exit the script.' '\n' ...
+        'Response: ' ]);
+    else
+      reward_bowl_prep = input(['\n\n' ...
+        'Left Bowl = ' initialization_struct.left_item '\n'...
+        'Right Bowl = ' initialization_struct.right_item '\n\n' ...
+        '1 = Bowls are set up; continue.' '\n' ...
+        '0 = I need to fix something; exit the script.' '\n' ...
+        'Response: ' ]);
+    end
+
+    if ~ismember(reward_bowl_prep, [0 1])
+        disp('Invalid entry, please try again.')
+        sca; return
+    elseif reward_bowl_prep == 0
+        disp([fprintf('\n') ...
+        'OK, you should restart the function to try again'])
+        sca; return
+    end
+
+% --- run the task
+    exit_flag = main_task(initialization_struct, initialization_struct.num_trials(2), initialization_struct.block(2));
+
+    if exit_flag == 1
+        disp('The script was exited because ESCAPE was pressed')
+        sca; return
+    end
+end
+
+% --- display winnings
+load([data_file_path sl 'money.mat']);
+fprintf('\n\n\n\n\n\n\n\n\n\nThe participant earned $ %.2f during the money rounds', money_struct.payoff_sum);
+fprintf('\nRound up to the nearest dollar and pay them (show up fee included) = $ %.2f', money_struct.payoff_total);
+
+end
